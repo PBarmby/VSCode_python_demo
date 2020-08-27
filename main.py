6b7fbcae9a0ac366658d8ce8d282841a81287eef
Pauline Barmby <pbarmby@uwo.ca>
2020-08-27 12:28:17 -0400

    added plotting code and plot

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-print("hello world")
-for i in range(1,11):
-    print(2*i)
+import matplotlib.pyplot as plt
+import numpy as np
+
+xval = np.arange(0, np.pi*4, np.pi*4/50)
+yval = np.cos(xval)
+
+#print(xval, yval)
+
+figure, ax = plt.subplots()
+ax.scatter(xval,yval)
+ax.set_xlabel(r'$\theta$ [rad]')
+ax.set_ylabel(r'cos($\theta$)')
+plt.show()
