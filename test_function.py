c25272de0e7858b85ee3362f0e39fb33e3ea8a0e
Pauline Barmby <pbarmby@uwo.ca>
2020-09-12 12:51:21 -0400

    added fahr_to_celsius

--- test_function.py
+++ test_function.py
@@ -11,3 +11,14 @@ print(y)
 # %%
 testfunc(3)
 # %%
+def fahr_to_celsius(temp_f):
+    return((temp_f - 32) * (5/9))
+# %%
+fahr_to_celsius(98)
+# %%
+freeze = fahr_to_celsius(32)
+# %%
+print(freeze)
+# %%
+print('Boiling point:', fahr_to_celsius(212), "C")
+# %%
