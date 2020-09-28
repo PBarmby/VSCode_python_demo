b96b7f7785941a7573399ceb1c049229ad7516b4
Pauline Barmby <pbarmby@uwo.ca>
2020-09-28 10:54:44 -0400

    added demo of solve_ivp

--- integration_odes.py
+++ integration_odes.py
@@ -57,3 +57,57 @@ def myfunc3(x):
     return(3*np.exp(x))
 scint.quad(myfunc3,0,np.inf)
 # %%
+help(scint.solve_ivp)
+# %%
+# ODE dy/dt = -2 y between t = 0 and 4,
+#  with the initial condition y(t=0) = 1
+# %%
+def dydt(t, y):
+    return(-2.0*y)
+# %%
+result = scint.solve_ivp(dydt, (0,4), np.array([1.0]))
+print(result)
+# %%
+%matplotlib inline
+fig, ax = plt.subplots()
+ax.plot(result.t, result.y[0])
+# %%
+print(result.t)
+# %%
+t_soln = np.linspace(0,4,100)
+result = scint.solve_ivp(dydt, (0,4), np.array([1.0]), t_eval=t_soln)
+print(result)
+# %%
+#The position of a mass on a spring obeys the 2nd order ODE 
+# $y'' + 2 \varepsilon \omega_0 y' + \omega_0^2 y = 0$ 
+# with $\omega_0^2 = k/m$ with $k$ the spring constant, 
+# $m$ the mass and $\varepsilon = c/(2 m \omega_0)$ 
+# with $c$ the damping coefficient.
+# %%
+# define some variables and compute some constants
+mass = 0.5  # kg
+kspring = 4  # N/m
+cviscous = 0.4  # N s/m
+eps = cviscous / (2 * mass * np.sqrt(kspring/mass))
+omega = np.sqrt(kspring / mass)
+# %%
+# state vector [y, yprime]
+# dydt function returns [yprime, yprimeprime]
+def deriv_ystate(time, ystate, epsilon, omega_spring):
+    yprime = ystate[1]
+    y2prime = -2.0 * epsilon * omega_spring * ystate[1] - omega_spring **2 * ystate[0]
+    return(yprime, y2prime)
+
+# %%
+time_soln = np.linspace(0,10,100)
+yinit = (1,0)
+result = scint.solve_ivp (deriv_ystate, (0,10), yinit, t_eval = t_soln, args = (eps, omega,))
+# %%
+print(result)
+# %%
+fig, ax = plt.subplots()
+ax.plot(result.t, result.y[0])
+# %%
+fig, ax = plt.subplots()
+ax.plot(result.t, result.y[1])
+# %%
