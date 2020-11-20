284d7c57f044245d9d820866a143532d02e12c02
Pauline Barmby <pbarmby@uwo.ca>
2020-11-20 11:22:10 -0500

    add implicit scheme

--- week12-FTCS-Diffusion_mtx.py
+++ week12-FTCS-Diffusion_mtx.py
@@ -1,33 +1,12 @@
 # To add a new cell, type '# %%'
 # To add a new markdown cell, type '# %% [markdown]'
-# %%
-from IPython import get_ipython
 
-# %% [markdown]
-# #  FTCS Diffusion - matrix formulation
 
-# %%
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
-get_ipython().run_line_magic('matplotlib', 'inline')
 
-# %% [markdown]
-# ### Background 
-# 
-# We have a metal bar of thermal conductivity _&kappa;_. If _T (x, t)_ is the temperature of position _x_ in the bar at time _t_, the time evolution of the temperature is governed by the diffusion equation,
-# 
-# _&part;T (x, t)/&part;t = &kappa; &part;<sup>2</sup>T (x, t)/&part;x<sup>2</sup>_
-# 
-# From an initial condition at _t = 0_, the temperature must be integrated forward in time subject to boundary conditions at the ends. Here we'll use the FTCS (forward time, centred space) algorithm to solve this problem. An important feature of this, and all explicit methods, is that there is a maximum time step above which the method becomes unstable. The FTCS method for the diffusion equation is only stable if the time-step _&tau;_ satisfies
-# 
-# _&tau; &le; t<sub>&sigma;</sub> &equiv; h<sup>2</sup>/(2 &kappa;)_
-# 
-# where _h_ is the spatial grid size.
-# %% [markdown]
-# ### Part 1
-# 
 # Adaptation of the `dftcs` program from the textbook into a function that will integrate the 1D diffusion equation in the FTCS approximation forward in time.
 
 # %%
@@ -73,7 +52,7 @@ def diffusion_ftcs(nspace, ntime, tau_rel, args = [1.0, 1.0]):
 # Adaptation of the above function into a version that uses the matrix formulation.
 
 # %%
-def diffusion_ftcs_mtx(nspace, ntime, tau_rel, args = [1.0, 1.0]):
+def diffusion_ftcs_mtx(nspace, ntime, tau_rel, implicit=True, args = [1.0, 1.0]):
     """ 
         Compute the solution to the diffusion equation using the forward-time, centered-space algorithm in matrix formulation
         nspace: number of spatial grid points
@@ -106,7 +85,10 @@ def diffusion_ftcs_mtx(nspace, ntime, tau_rel, args = [1.0, 1.0]):
     D[0] = 0 
     D[-1] = 0
     
-    A = np.identity(nspace) + coeff*D 
+    if implicit:
+        A = np.linalg.inv(np.identity(nspace) - coeff*D)
+    else: # explicit FTCS 
+        A = np.identity(nspace) + coeff*D 
     
     for istep in range(1, ntime):  ## MAIN LOOP: note it starts at 1, not zero: istep = 0 is the IC #
         #* Compute new temperature using FTCS scheme.
@@ -119,15 +101,12 @@ def diffusion_ftcs_mtx(nspace, ntime, tau_rel, args = [1.0, 1.0]):
 h=1/60.0
 tsig = h**2/(2) # compute t_sigma
 trel = 1e-4/tsig # compute trel
-tt = diffusion_ftcs(61, 300, trel)
-tt_mtx = diffusion_ftcs_mtx(61, 300, trel)
 xplot = np.arange(61)*h - 0.5
 tplot = np.arange(300)*(trel*tsig) 
 
-
 # %%
-print((tt-tt_mtx).sum())
-
+tt = diffusion_ftcs(61, 300, trel)
+tt_mtx = diffusion_ftcs_mtx(61, 300, trel)
 
 # %%
 print(np.abs(tt-tt_mtx).sum())
